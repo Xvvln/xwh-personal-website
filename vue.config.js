f5f05aa48a038e4a6f4c15eab86bc48aa2c7a960
Xvvln <3369759202@qq.com>
2025-03-27 13:48:25 +0800

    尝试解决vercel部署

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,30 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: '/',
   productionSourceMap: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: 6,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `npm.${packageName.replace('@', '')}`;
+            },
+            priority: 10
+          }
+        }
+      }
+    },
+    performance: {
+      hints: false
+    }
+  },
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = 'xwh - 前端开发工程师';
